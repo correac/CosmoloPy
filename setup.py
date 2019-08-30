4ebcf147b59a289a7bd5c821b099f63e31af2621
Camila Correa <correa@strw.leidenuniv.nl>
2019-08-30 16:38:42 +0200

    Updating setup, removing C dependency, cleaning .., moving to Python3.x

--- setup.py
+++ setup.py
@@ -48,9 +48,6 @@ Capabilities include
   
 """,
     classifiers = ['License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.6',
-                   'Topic :: Scientific/Engineering :: Astronomy',
-                   'Operating System :: OS Independent'
-                   ]
+                   'Programming Language :: Python :: 3.7',
+                   'Topic :: Astronomy']
 )
